985f9faae07e6c9ddf312bf9bc6ddfc41c7efbda
kylin <whenhan@foxmail.com>
2018-06-14 15:55:22 +0800

    * 使用示例

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,66 +1,115 @@
-'use strict';
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.DB = factory());
+}(this, (function () { 'use strict';
 
-var version = "0.0.1";
+  var version = "0.0.1";
 
-function Storage(type) {
-    this.version = version;
-    const storage = type === "local" ? localStorage : sessionStorage;
-    const set = (name$$1, data) => {
-      const type = Object.prototype.toString.call(data);
-      if (storage[name$$1]) {
-        switch (type) {
-          case "[object Array]":
-            storage[name$$1] = JSON.stringify(data);
-            break;
-          case "[object JSON]":
-          case "[object Object]":
-            storage[name$$1] = JSON.stringify({
-              ...JSON.parse(storage[name$$1]),
-              ...data
-            });
-            break;
-          case "[object String]":
-            storage[name$$1] = data;
-            break;
-          case "[object Number]":
-          case "[object Null]":
-          case "[object Undefined]":
-          default:
-            storage[name$$1] = data.toString();
+  var indexDB = () => {};
+
+  const dataType = data => Object.prototype.toString.call(data);
+  function isArray(data) {
+    return dataType(data) === "[object Array]";
+  }
+  function isObject(data) {
+    return dataType(data) === "[object Object]";
+  }
+  function isNull(data) {
+    return dataType(data) === "[object Null]";
+  }
+  function isUndefined(data) {
+    return dataType(data) === "[object Undefined]";
+  }
+  function isJSON(data) {
+    return window.JSON && dataType(data) === "[object JSON]";
+  }
+  function isBoolean(data) {
+    return dataType(data) === "[object Boolean]";
+  }
+
+  function storage(type = "session") {
+    const storage = window[`${type}Storage`];
+    const set = (name, data) => {
+      if (isArray(data) || isJSON(data) || isObject(data)) {
+        storage[name] = _serialize(data);
+        return;
+      }
+      if (isBoolean(data) || isNull(data) || isUndefined(data)) {
+        storage[name] = data;
+        return;
+      }
+      storage[name] = data.toString();
+    };
+
+    // 与本地新旧数据合并
+    const merge = (name, data) => {
+      // 如果是数组或JSON 对象则数据可以合并，否则直接替换
+      if (isArray(data) || isJSON(data) || isObject(data)) {
+        if (isArray(data)) {
+          storage[name] = _serialize([..._deserialize(storage[name]), ...data]);
+        } else {
+          storage[name] = _serialize({ ..._deserialize(storage[name]), ...data });
         }
       } else {
-        switch (type) {
-          case "[object Array]":
-          case "[object JSON]":
-          case "[object Object]":
-            storage[name$$1] = JSON.stringify(data);
-            break;
-          case "[object String]":
-            storage[name$$1] = data;
-            break;
-          case "[object Number]":
-          case "[object Null]":
-          case "[object Undefined]":
-          default:
-            storage[name$$1] = data.toString();
-        }
+        set(name, data);
       }
     };
-    const del = name$$1 => storage.removeItem(name$$1);
-    const get = name$$1 => {
-      if (storage[name$$1]) {
-        try {
-          return JSON.parse(storage[name$$1]);
-        } catch (e) {
-          return storage[name$$1];
-        }
+
+    const _serialize = obj => JSON.stringify(obj);
+    const _deserialize = strVal => {
+      if (!strVal) return strVal;
+      let val = "";
+      try {
+        val = JSON.parse(strVal);
+      } catch (e) {
+        val = strVal;
       }
+      return val;
+    };
+
+    // 删除数据
+    const _remove = name => storage.removeItem(name);
+    // 删除一项或多项数据
+    const del = name => {
+      if (Array.isArray(name)) {
+        name.forEach(key => _remove(key));
+      } else {
+        _remove(name);
+      }
+    };
+    // 清除所有数据
+    const clear = () => storage.clear();
+
+    // 读取本地数据
+    const get = name => {
+      if (Array.isArray(name)) {
+        const obj = {};
+        name.forEach(key => {
+          obj[key] = _deserialize(storage[key]);
+        });
+
+        return obj;
+      }
+
+      return _deserialize(storage[name]);
     };
     return {
-      set: set,
-      get: get,
-      del: del
+      set,
+      get,
+      del,
+      clear,
+      merge
     };
   }
 
-module.exports = Storage;
+  const DB = {
+    version,
+    index: indexDB,
+    session: storage("session"),
+    local: storage("local")
+  };
+
+  return DB;
+
+})));
